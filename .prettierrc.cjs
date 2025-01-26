0d8e0d185736bfeb1f666bb0a9eede55bf8f87ff
ypezoadev <ypezoadev@gmail.com>
2025-01-26 05:09:30 -0300

    first commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  jsxSingleQuote: true,
+  singleQuote: true,
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 100,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+}
