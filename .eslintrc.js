62befadfaa6cc40b76e4352fa49ca8744a85c416
ypezoadev <ypezoadev@gmail.com>
2025-01-28 00:27:18 -0300

    added eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: ['expo', 'prettier'],
+  plugins: ['prettier'],
   ignorePatterns: ['/dist/*', 'node_modules'],
   rules: {
-    "prettier/prettier": 'error',
-  }
-};
+    'prettier/prettier': 'error',
+  },
+}
